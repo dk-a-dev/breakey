58a2e325ffdc5b3a15d09d679192aba6f33a1cff
Dev Keshwani <dev.keshwani345@gmail.com>
2024-04-22 08:34:09 +0530

    fix: bug fix

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,8 +1,7 @@
 import express from 'express';
-import { registerUser, loginUser, logoutUser } from '../controller/userController.js';
+import { registerUser, loginUser, logoutUser} from '../controller/userController.js';
 import { isUserAuthenticated} from '../middleware/authCheck.js';
 
-// import {isUserAuthenticated} from '../middleware/authCheck.js';
 const router=express.Router();
 router.post("/register",registerUser);
 router.post("/login",loginUser);
